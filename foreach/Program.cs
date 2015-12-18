497f3a3008be642f3ca9e71b248811f4b6b88a61
Ikram_Lim <ikramlim@gmail.com>
2015-12-18 17:51:35 +0800

    foreach loop
    foreach loop

---
+++ foreach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[5]; //declaring array 
+
+            //storing value in array element
+            a[0] = "Steven";
+            a[1] = "Clark";
+            a[2] = "Mark";
+            a[3] = "Thompson";
+            a[4] = "John";
+
+            //retrieving value using foreach loop
+            foreach (string name in a)
+            {
+                Console.WriteLine("Hello, Good Morning " + name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*
+    Hello, Good Morning Steven
+    Hello, Good Morning Clark
+    Hello, Good Morning Mark
+    Hello, Good Morning Thompson
+    Hello, Good Morning John
+*/
